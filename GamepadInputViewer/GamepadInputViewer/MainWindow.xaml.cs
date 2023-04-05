4a35c989fe8f40fba65dde2774c39e0788c1689c
Sylwester Piskozub <sylwpis025@student.polsl.pl>
2023-04-05 12:46:03 +0200

    improved joysticks and improved color update code

--- GamepadInputViewer/GamepadInputViewer/MainWindow.xaml.cs
+++ GamepadInputViewer/GamepadInputViewer/MainWindow.xaml.cs
@@ -8,21 +8,31 @@ using System.Windows.Controls;
 using SharpDX.DirectInput;
 using GamepadInputViewer.Controllers;
 using GamepadInputViewer.Model;
+using System.Windows.Shapes;
 
 namespace GamepadInputViewer
 {
     public partial class MainWindow : Window
     {
+        Color BUTTON_PRESSED = Colors.Green;
+        Color BUTTON_NOT_PRESSED = Colors.Gray;
+        Color DEVICE_ACTIVE = Colors.Green;
+        Color DEVICE_NOT_ACTIVE = Colors.Red;
+        int MAX_AXIS = 32768;
+        int THUMB_PADDING = 22;
+        Tuple<double, double> leftThumbPosition;
+        Tuple<double, double> rightThumbPosition;
         GamepadBase gamepad = null;
         Controller? controller = null;
         DeviceManagerXInput deviceManager = new DeviceManagerXInput();
         State currentState;
         State previousState;
-        short triggerThreshold = 5000;
         public bool Checked { get; set; } = true;
         public MainWindow()
         {
             InitializeComponent();
+            leftThumbPosition = new Tuple<double, double>(LeftThumbPos.Margin.Top, LeftThumbPos.Margin.Left);
+            rightThumbPosition = new Tuple<double, double>(RightThumbPos.Margin.Top, RightThumbPos.Margin.Left);
             gamepad = new GamePadXInput(deviceManager.GetController());
             Autoconnect.DataContext = this;
             Task.Run(async () =>
@@ -124,203 +134,56 @@ namespace GamepadInputViewer
 
         private void updateDeviceView()
         {
-            if (deviceManager.isControllerConnected(0))
-            {
-                paintElipse(Device1, Colors.Green);
-            }
-            else
-            {
-                paintElipse(Device1, Colors.Red);
-            }
-            if (deviceManager.isControllerConnected(1))
-            {
-                paintElipse(Device2, Colors.Green);
-            }
-            else
-            {
-                paintElipse(Device2, Colors.Red);
-            }
-            if (deviceManager.isControllerConnected(2))
-            {
-                paintElipse(Device3, Colors.Green);
-            }
-            else
-            {
-                paintElipse(Device3, Colors.Red);
-            }
-            if (deviceManager.isControllerConnected(3))
-            {
-                paintElipse(Device4, Colors.Green);
-            }
-            else
-            {
-                paintElipse(Device4, Colors.Red);
-            }
+            updateDeviceColor(0, Device1);
+            updateDeviceColor(1, Device2);
+            updateDeviceColor(2, Device3);
+            updateDeviceColor(3, Device4);
         }
 
         public void updateGamepadView()
         {
-            //buttons
-            if (gamepad.isTopButtonPressed())
-            {
-                paintElipse(ButtonY, Colors.Green);
-            } else
-            {
-                paintElipse(ButtonY, Colors.Gray);
-            }
+            updateButtonColor(gamepad.isTopButtonPressed(), ButtonY);
+            updateButtonColor(gamepad.isRightButtonPressed(), ButtonB);
+            updateButtonColor(gamepad.isBottomButtonPressed(), ButtonA);
+            updateButtonColor(gamepad.isLeftButtonPressed(), ButtonX);
+            updateButtonColor(gamepad.isStartButtonPressed(), Start);
+            updateButtonColor(gamepad.isBackButtonPressed(), Back);
+            updateButtonColor(gamepad.isDPadDownPressed(), DPadDown);
+            updateButtonColor(gamepad.isDPadLeftPressed(), DPadLeft);
+            updateButtonColor(gamepad.isDPadRightPressed(), DPadRight);
+            updateButtonColor(gamepad.isDPadUpPressed(), DPadUp);
+            updateButtonColor(gamepad.isLeftJoystickPressed(), LeftThumb);
+            updateButtonColor(gamepad.isRightJoystickPressed(), RightThumb);
+            updateButtonColor(gamepad.isLeftBumperPressed(), LeftShoulder);
+            updateButtonColor(gamepad.isRightBumperPressed(), RightShoulder);
+            updateTriggerColor(gamepad.getLeftTrigger(), LeftTrigger); 
+            updateTriggerColor(gamepad.getRightTrigger(), RightTrigger);
 
-            if (gamepad.isRightButtonPressed())
-            {
-                paintElipse(ButtonB, Colors.Green);
-            }
-            else
-            {
-                paintElipse(ButtonB, Colors.Gray);
-            }
+            updateLeftThumbPos();
+            updateRightThumbPos();
 
-            if (gamepad.isBottomButtonPressed())
-            {
-                paintElipse(ButtonA, Colors.Green);
-            }
-            else
-            {
-                paintElipse(ButtonA, Colors.Gray);
-            }
+        }
 
-            if (gamepad.isLeftButtonPressed())
-            {
-                paintElipse(ButtonX, Colors.Green);
-            }
-            else
-            {
-                paintElipse(ButtonX, Colors.Gray);
-            }
+        private void updateLeftThumbPos()
+        {
+            int devider = MAX_AXIS / THUMB_PADDING;
+            var thumbAxes = gamepad.getLeftJoystickAxes();
+            int displacementXAxis = thumbAxes.Item1 / devider;
+            displacementXAxis = (displacementXAxis > THUMB_PADDING) ? THUMB_PADDING : displacementXAxis;
+            int displacementYAxis = thumbAxes.Item2 / devider;
+            displacementYAxis = (displacementYAxis > THUMB_PADDING) ? THUMB_PADDING : displacementYAxis;
+            LeftThumbPos.Dispatcher.BeginInvoke((Action)(() => LeftThumbPos.Margin = new Thickness(leftThumbPosition.Item1 + displacementXAxis, leftThumbPosition.Item2 - displacementYAxis, 0, 0)));
+        }
 
-            if (currentState.Gamepad.Buttons == GamepadButtonFlags.B)
-            {
-                ButtonB.Dispatcher.BeginInvoke((Action)(() => ButtonB.Fill = new SolidColorBrush(Colors.Green)));
-            }
-            else if (currentState.Gamepad.Buttons == GamepadButtonFlags.A)
-            {
-                ButtonA.Dispatcher.BeginInvoke((Action)(() => ButtonA.Fill = new SolidColorBrush(Colors.Green)));
-            }
-            else if (currentState.Gamepad.Buttons == GamepadButtonFlags.X)
-            {
-                ButtonX.Dispatcher.BeginInvoke((Action)(() => ButtonX.Fill = new SolidColorBrush(Colors.Green)));
-            }
-            else if (currentState.Gamepad.Buttons == GamepadButtonFlags.RightThumb)
-            {
-                RightThumb.Dispatcher.BeginInvoke((Action)(() => RightThumb.Fill = new SolidColorBrush(Colors.Green)));
-            }
-            else if (currentState.Gamepad.Buttons == GamepadButtonFlags.LeftThumb)
-            {
-                LeftThumb.Dispatcher.BeginInvoke((Action)(() => LeftThumb.Fill = new SolidColorBrush(Colors.Green)));
-            }
-            else if (currentState.Gamepad.Buttons == GamepadButtonFlags.Start)
-            {
-                Start.Dispatcher.BeginInvoke((Action)(() => Start.Fill = new SolidColorBrush(Colors.Green)));
-            }
-            else if (currentState.Gamepad.Buttons == GamepadButtonFlags.Back)
-            {
-                Back.Dispatcher.BeginInvoke((Action)(() => Back.Fill = new SolidColorBrush(Colors.Green)));
-            }
-            else if (currentState.Gamepad.Buttons == GamepadButtonFlags.LeftShoulder)
-            {
-                LeftShoulder.Dispatcher.BeginInvoke((Action)(() => LeftShoulder.Fill = new SolidColorBrush(Colors.Green)));
-            }
-            else if (currentState.Gamepad.Buttons == GamepadButtonFlags.RightShoulder)
-            {
-                RightShoulder.Dispatcher.BeginInvoke((Action)(() => RightShoulder.Fill = new SolidColorBrush(Colors.Green)));
-            }
-            else if (currentState.Gamepad.Buttons == GamepadButtonFlags.DPadUp)
-            {
-                DPadUp.Dispatcher.BeginInvoke((Action)(() => DPadUp.Fill = new SolidColorBrush(Colors.Green)));
-            }
-            else if (currentState.Gamepad.Buttons == GamepadButtonFlags.DPadRight)
-            {
-                DPadRight.Dispatcher.BeginInvoke((Action)(() => DPadRight.Fill = new SolidColorBrush(Colors.Green)));
-            }
-            else if (currentState.Gamepad.Buttons == GamepadButtonFlags.DPadDown)
-            {
-                DPadDown.Dispatcher.BeginInvoke((Action)(() => DPadDown.Fill = new SolidColorBrush(Colors.Green)));
-            }
-            else if (currentState.Gamepad.Buttons == GamepadButtonFlags.DPadLeft)
-            {
-                DPadLeft.Dispatcher.BeginInvoke((Action)(() => DPadLeft.Fill = new SolidColorBrush(Colors.Green)));
-            }
-            //triggers
-            if (currentState.Gamepad.LeftTrigger == 0)
-            {
-                LeftTrigger.Dispatcher.BeginInvoke((Action)(() => LeftTrigger.Fill = new SolidColorBrush(Colors.Gray)));
-            }
-            else
-            {
-                LeftTrigger.Dispatcher.BeginInvoke((Action)(() => LeftTrigger.Fill = new SolidColorBrush(Colors.Green)));
-            }
-
-            if (currentState.Gamepad.RightTrigger == 0)
-            {
-                RightTrigger.Dispatcher.BeginInvoke((Action)(() => RightTrigger.Fill = new SolidColorBrush(Colors.Gray)));
-            }
-            else
-            {
-                RightTrigger.Dispatcher.BeginInvoke((Action)(() => RightTrigger.Fill = new SolidColorBrush(Colors.Green)));
-            }
-            //leftthumb
-            if (currentState.Gamepad.LeftThumbX < triggerThreshold && currentState.Gamepad.LeftThumbX > -triggerThreshold)
-            {
-                LeftThumbLeft.Dispatcher.BeginInvoke((Action)(() => LeftThumbLeft.Fill = new SolidColorBrush(Colors.Gray)));
-                LeftThumbRight.Dispatcher.BeginInvoke((Action)(() => LeftThumbRight.Fill = new SolidColorBrush(Colors.Gray)));
-            }
-            else if (currentState.Gamepad.LeftThumbX > triggerThreshold)
-            {
-                LeftThumbRight.Dispatcher.BeginInvoke((Action)(() => LeftThumbRight.Fill = new SolidColorBrush(Colors.Green)));
-            }
-            else if (currentState.Gamepad.LeftThumbX < -triggerThreshold)
-            {
-                LeftThumbLeft.Dispatcher.BeginInvoke((Action)(() => LeftThumbLeft.Fill = new SolidColorBrush(Colors.Green)));
-            }
-            if (currentState.Gamepad.LeftThumbY < triggerThreshold && currentState.Gamepad.LeftThumbY > -triggerThreshold)
-            {
-                LeftThumbDown.Dispatcher.BeginInvoke((Action)(() => LeftThumbDown.Fill = new SolidColorBrush(Colors.Gray)));
-                LeftThumbUp.Dispatcher.BeginInvoke((Action)(() => LeftThumbUp.Fill = new SolidColorBrush(Colors.Gray)));
-            }
-            else if (currentState.Gamepad.LeftThumbY > triggerThreshold)
-            {
-                LeftThumbUp.Dispatcher.BeginInvoke((Action)(() => LeftThumbUp.Fill = new SolidColorBrush(Colors.Green)));
-            }
-            else if (currentState.Gamepad.LeftThumbY < -triggerThreshold)
-            {
-                LeftThumbDown.Dispatcher.BeginInvoke((Action)(() => LeftThumbDown.Fill = new SolidColorBrush(Colors.Green)));
-            }
-
-            //rightthumb
-            if (currentState.Gamepad.RightThumbX < triggerThreshold && currentState.Gamepad.RightThumbX > -triggerThreshold)
-            {
-                RightThumbLeft.Dispatcher.BeginInvoke((Action)(() => RightThumbLeft.Fill = new SolidColorBrush(Colors.Gray)));
-                RightThumbRight.Dispatcher.BeginInvoke((Action)(() => RightThumbRight.Fill = new SolidColorBrush(Colors.Gray)));
-            }
-            else if (currentState.Gamepad.RightThumbX > triggerThreshold)
-            {
-                RightThumbRight.Dispatcher.BeginInvoke((Action)(() => RightThumbRight.Fill = new SolidColorBrush(Colors.Green)));
-            }
-            else if (currentState.Gamepad.RightThumbX < -triggerThreshold)
-            {
-                RightThumbLeft.Dispatcher.BeginInvoke((Action)(() => RightThumbLeft.Fill = new SolidColorBrush(Colors.Green)));
-            }
-            if (currentState.Gamepad.RightThumbY < triggerThreshold && currentState.Gamepad.RightThumbY > -triggerThreshold)
-            {
-                RightThumbDown.Dispatcher.BeginInvoke((Action)(() => RightThumbDown.Fill = new SolidColorBrush(Colors.Gray)));
-                RightThumbUp.Dispatcher.BeginInvoke((Action)(() => RightThumbUp.Fill = new SolidColorBrush(Colors.Gray)));
-            }
-            else if (currentState.Gamepad.RightThumbY > triggerThreshold)
-            {
-                RightThumbUp.Dispatcher.BeginInvoke((Action)(() => RightThumbUp.Fill = new SolidColorBrush(Colors.Green)));
-            }
-            else if (currentState.Gamepad.RightThumbY < -triggerThreshold)
-            {
-                RightThumbDown.Dispatcher.BeginInvoke((Action)(() => RightThumbDown.Fill = new SolidColorBrush(Colors.Green)));
-            }
+        private void updateRightThumbPos()
+        {
+            int devider = MAX_AXIS / THUMB_PADDING;
+            var thumbAxes = gamepad.getRightJoystickAxes();
+            int displacementXAxis = thumbAxes.Item1 / devider;
+            displacementXAxis = (displacementXAxis > THUMB_PADDING) ? THUMB_PADDING : displacementXAxis;
+            int displacementYAxis = thumbAxes.Item2 / devider;
+            displacementYAxis = (displacementYAxis > THUMB_PADDING) ? THUMB_PADDING : displacementYAxis;
+            RightThumbPos.Dispatcher.BeginInvoke((Action)(() => RightThumbPos.Margin = new Thickness(rightThumbPosition.Item2 + displacementXAxis, rightThumbPosition.Item1 - displacementYAxis, 0, 0)));
         }
 
         private void refreshDevices()
@@ -349,9 +212,45 @@ namespace GamepadInputViewer
             controller = deviceManager.GetController(DeviceSelector.SelectedIndex);
         }
 
-        private void paintElipse(System.Windows.Shapes.Ellipse elipse, System.Windows.Media.Color color)
+        private void paintElipse (Ellipse elipse, Color color)
         {
             elipse.Dispatcher.BeginInvoke((Action)(() => elipse.Fill = new SolidColorBrush(color)));
+        }
+
+        private void updateButtonColor(bool isActive, Ellipse elipse)
+        {
+            if (isActive)
+            {
+                paintElipse(elipse, BUTTON_PRESSED);
+            }
+            else
+            {
+                paintElipse(elipse, BUTTON_NOT_PRESSED);
+            }
+        }
+
+        private void updateTriggerColor(int value, Ellipse elipse)
+        {
+            if (value > Gamepad.TriggerThreshold)
+            {
+                paintElipse(elipse, BUTTON_PRESSED);
+            }
+            else
+            {
+                paintElipse(elipse, BUTTON_NOT_PRESSED);
+            }
+        }
+
+        private void updateDeviceColor(int deviceId, Ellipse elipse)
+        {
+            if (deviceManager.isControllerConnected(deviceId))
+            {
+                paintElipse(elipse, DEVICE_ACTIVE);
+            }
+            else
+            {
+                paintElipse(elipse, DEVICE_NOT_ACTIVE);
+            }
         }
     }
 }

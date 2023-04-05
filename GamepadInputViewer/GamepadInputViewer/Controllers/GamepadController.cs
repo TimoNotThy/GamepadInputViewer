02fc658d334cc75e488b84a31eac26af49fa58ee
Sylwester Piskozub <sylwpis025@student.polsl.pl>
2023-04-05 13:40:13 +0200

    moved function

--- GamepadInputViewer/GamepadInputViewer/Controllers/GamepadController.cs
+++ GamepadInputViewer/GamepadInputViewer/Controllers/GamepadController.cs
@@ -8,5 +8,63 @@ namespace GamepadInputViewer.Controllers
 {
     internal class GamepadController
     {
+
+/*        static void MainForJoystick()
+        {
+            // Initialize DirectInput
+            var directInput = new DirectInput();
+
+            // Find a Joystick Guid
+            var joystickGuid = Guid.Empty;
+
+            foreach (var deviceInstance in directInput.GetDevices(SharpDX.DirectInput.DeviceType.Gamepad, DeviceEnumerationFlags.AllDevices))
+                joystickGuid = deviceInstance.InstanceGuid;
+
+            // If Gamepad not found, look for a Joystick
+            if (joystickGuid == Guid.Empty)
+                foreach (var deviceInstance in directInput.GetDevices(SharpDX.DirectInput.DeviceType.Joystick, DeviceEnumerationFlags.AllDevices))
+                    joystickGuid = deviceInstance.InstanceGuid;
+
+            // If Joystick not found, throws an error
+            if (joystickGuid == Guid.Empty)
+            {
+                Trace.WriteLine("No joystick/Gamepad found.");
+            }
+
+            // Instantiate the joystick
+            var joystick = new Joystick(directInput, joystickGuid);
+
+            Trace.WriteLine("Found Joystick/Gamepad with GUID: " + joystickGuid);
+
+            // Query all suported ForceFeedback effects
+            var allEffects = joystick.GetEffects();
+            foreach (var effectInfo in allEffects)
+                Trace.WriteLine("Effect available " + effectInfo.Name);
+
+            // Set BufferSize in order to use buffered data.
+            joystick.Properties.BufferSize = 128;
+
+            // Acquire the joystick
+            joystick.Acquire();
+
+            // Poll events from joystick
+            while (true)
+            {
+                joystick.Poll();
+                var datas = joystick.GetBufferedData();
+                var buttons = joystick.GetCurrentState().Buttons;
+                Trace.WriteLine(joystick.GetCurrentState().PointOfViewControllers.GetValue(0));
+                *//*                foreach (var data in datas)
+                                {
+                                    Trace.WriteLine(data);
+                                }*/
+                /*                for (int i = 0; i < buttons.Length; i++)
+                                {
+                                    if (buttons[i] == true)
+                                        Trace.WriteLine(i);
+                                }*//*
+
+            }
+        }*/
     }
 }
